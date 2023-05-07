1c06bc3b1a4bbdaf2bf715049a428a961dd70d68
Alfredo Perez <alfredo.perez.q@gmail.com>
2023-05-07 17:54:20 -0500

    updates storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,19 @@
 import type { StorybookConfig } from '@storybook/core-common';
-
 export const rootMain: StorybookConfig = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials']
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
   //   // Return the altered config
   //   return config;
   // },
+};
+
+export const framework = {
+  name: '@storybook/angular',
+  options: {}
+};
+export const docs = {
+  autodocs: true
 };

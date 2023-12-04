7fbd4cddf3b27d17b799d278db412b6e000357e1
Alfredo Perez <alfredo.perez.q@gmail.com>
2023-12-04 06:36:42 -0600

    Adds API Service base class

--- libs/demos/src/lib/demos/api-service-demo/services/api.service.ts
+++ libs/demos/src/lib/demos/api-service-demo/services/api.service.ts
@@ -1,9 +1,71 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiService {
+class PaginationQueryDto {
+  limit!: number;
 
-  constructor() { }
+  page!: number;
+
+  orderDirection!: 'ASC' | 'DESC';
+
+  orderBy!: string;
+}
+
+export abstract class ApiService<T> {
+  private httpClient = inject(HttpClient);
+
+  protected constructor(private entityName: string) {}
+
+  public getAll(paginationQueryDto?: PaginationQueryDto): Observable<T[]> {
+    return this.request('GET', paginationQueryDto);
+  }
+
+  public get(id: number): Observable<T> {
+    return this.request('GET', undefined, undefined, id);
+  }
+
+  public create(body: Partial<T>): Observable<T> {
+    return this.request('POST', undefined, body);
+  }
+
+  public update(id: number, body: Partial<T>): Observable<T> {
+    return this.request('PATCH', undefined, body, id);
+  }
+
+  protected request<T>(
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH',
+    paginationQuery?: PaginationQueryDto,
+    body?: any,
+    id?: number,
+  ) {
+    const url = this.getUrl(id);
+    const options = this.getOptions(paginationQuery, body);
+
+    return this.httpClient.request<T>(method, url, options);
+  }
+
+  private getUrl(id?: number) {
+    const idPath = !id ? '' : `/${id}`;
+    // TODO:  need to make this an environment variable
+    return `http://localhost:7000/${this.entityName}${idPath}`;
+  }
+
+  private getOptions(paginationQuery?: PaginationQueryDto, body?: any) {
+    let params = {};
+    if (paginationQuery) {
+      const paginationParams = {
+        limit: paginationQuery.limit.toString(),
+        page: paginationQuery.page.toString(),
+        orderBy: paginationQuery.orderBy,
+        orderDirection: paginationQuery.orderDirection,
+      };
+      params = { ...paginationParams };
+    }
+
+    return {
+      params,
+      body,
+    };
+  }
 }
